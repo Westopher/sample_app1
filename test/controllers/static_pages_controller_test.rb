2aea53306c753d87f9dc42bffbe3bd5cfb0e312e
West Kraemer <WestKraemer@gmail.com>
2015-10-27 19:13:12 -0400

    modify style sheet homepage

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,16 +5,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "West's Website"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "West's Website"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "West's Website"
   end
 
 end
